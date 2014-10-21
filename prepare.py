efb023d90960b06d1829ca312a57cd09b61795ab
Vytis Banaitis <vytis.banaitis@gmail.com>
2014-10-21 22:33:02 +0300

    Updated for SharedGroupThreshold score type

--- prepare.py
+++ prepare.py
@@ -61,7 +61,6 @@ test_dir = get_config_value('TEST_DIR', {'taskname':task_name})
 
 def get_test_case_defs():
     defs = get_config_value('TESTS')
-    defs = [(get_value(prefix, {'taskname':task_name}), filter, public) for prefix, filter, public in defs]
     return defs
 
 
@@ -76,70 +75,69 @@ class TestGroup(object):
     def __init__(self, cases=None, points=None):
         self.cases = cases or []
         self.points = points
+        self.threshold = 1.0
 
-input_re = re.compile(get_config_value('INPUT_SUFFIX'))
-output_re = re.compile(get_config_value('OUTPUT_SUFFIX'))
-
-def find_test_files(prefix):
-    inputs = {}
-    outputs = {}
+def find_test_files(test_dir, regexp):
+    test_files = []
+    r = re.compile(regexp)
+    get_output = get_config_value('OUTPUT_FILENAME')
     for fn in os.listdir(test_dir):
         path = os.path.join(test_dir, fn)
-        if not fn.startswith(prefix):
+        if not r.match(fn):
             continue
-        suffix = fn[len(prefix):]
-        for regex, store in [(input_re, inputs), (output_re, outputs)]:
-            m = regex.match(suffix)
-            if not m:
-                continue
-            group = m.group('testgroup')
-            try:
-                case = m.group('testcase')
-            except IndexError:
-                case = None
-            store[group, case] = path
-    return inputs, outputs
+        opath = get_output(path)
+        if not os.path.exists(opath):
+            continue
+        test_files.append((path, opath))
+    return test_files
 
 def collect_tests():
     defs = get_test_case_defs()
     gstore = defaultdict(TestGroup)
-    for prefix, filter_f, public_f in defs:
-        inputs, outputs = find_test_files(prefix)
-        cases = list(sorted(set(inputs.keys() + outputs.keys())))
-        for group, case in cases:
-            context = {'testgroup': group, 'testcase': case}
-            inc = get_value(filter_f, context)
-            if not inc:
-                continue
-            assert group, case in inputs
-            assert group, case in outputs
-            public = get_value(public_f, context)
+    test_cases = []
+    for dirpath, regexp, parse in defs:
+        test_files = find_test_files(dirpath, regexp)
+        for ipath, opath in test_files:
+            ret = parse(ipath)
+            if ret is not None:
+                case, public, groups = ret
+                test_cases.append((case, groups, TestCase(ipath, opath, public)))
+    test_cases.sort()
+    tests = []
+    for i, (case, grps, test) in enumerate(test_cases):
+        test.index = i
+        tests.append(test)
+        for group in grps:
             g = gstore[group]
-            c = TestCase(inputs[group, case], outputs[group, case], public)
-            g.cases.append(c)
+            g.cases.append(i)
+    print gstore.keys()
     groups = []
     for group in sorted(gstore.keys()):
-        groups.append(gstore[group])
-    return groups
+        g = gstore[group]
+        g.cases.sort()
+        groups.append(g)
+    return tests, groups
 
 
 def get_test_cases():
-    groups = collect_tests()
+    test_cases, groups = collect_tests()
     total_points = get_config_value('TOTAL_POINTS')
     points = get_config_value('POINTS',
                     {'total':total_points, 'n':len(groups)})
+    thresholds = get_config_value('THRESHOLDS')
     assert len(points) == len(groups)
     if total_points is not None:
         assert sum(points) == total_points
-    idx = 0
-    for g, p in zip(groups, points):
+    if thresholds is not None:
+        assert len(thresholds) == len(groups)
+    else:
+        thresholds = [1.0] * len(groups)
+    for g, p, t in zip(groups, points, thresholds):
         g.points = p
-        for c in g.cases:
-            c.index = idx
-            idx += 1
-    return groups
+        g.threshold = t
+    return test_cases, groups
 
-tests = get_test_cases()
+cases, groups = get_test_cases()
 
 
 def create_dirs():
@@ -148,30 +146,41 @@ def create_dirs():
 
 
 def copy_test_files():
-    for g in tests:
-        for c in g.cases:
-            copy(c.inputfile, 'input/input{}.txt'.format(c.index))
-            copy(c.outputfile, 'output/output{}.txt'.format(c.index))
+    for c in cases:
+        copy(c.inputfile, 'input/input{}.txt'.format(c.index))
+        copy(c.outputfile, 'output/output{}.txt'.format(c.index))
     os.system('flip -u input/* output/*')
 
 
 def copy_task():
-    src = get_config_value('TASK_DESCRIPTION', {'taskname':task_name})
-    copy(src, 'statement/statement.pdf')
+    languages = get_config_value('STATEMENT_LANGUAGES', default=[])
+    if languages:
+        found = []
+        for lang in languages:
+            src = get_config_value('TASK_DESCRIPTION', {'taskname':task_name, 'language':lang})
+            if os.path.isfile(src):
+                copy(src, 'statement/statement-{}.pdf'.format(lang))
+                found.append(lang)
+        return found
+    else:
+        src = get_config_value('TASK_DESCRIPTION', {'taskname':task_name})
+        copy(src, 'statement/statement.pdf')
 
 
 def write_scores():
-    with open('gen/GEN', 'w') as f:
-        for g in tests:
-            print >> f, '# ST: {}'.format(g.points)
-            for c in g.cases:
-                print >> f, os.path.basename(c.inputfile)
+    with open('gen/shared', 'w') as f:
+        for g in groups:
+            print >> f, 'ST: {} {}'.format(g.points, g.threshold)
+            for i in g.cases:
+                print >> f, '{}  # {}'.format(i, os.path.relpath(cases[i].inputfile, test_dir))
 
 
 def copy_api():
     api_dir = get_config_value('API_DIR', {'taskname': task_name})
-    api_private = os.path.join(api_dir, 'private')
-    api_public = os.path.join(api_dir, 'public')
+    api_private = get_config_value('API_DIR_PRIVATE', {'taskname': task_name},
+            default=os.path.join(api_dir, 'private'))
+    api_public = get_config_value('API_DIR_PUBLIC', {'taskname': task_name},
+            default=os.path.join(api_dir, 'public'))
     os.system('rm -r sol/* att/*')
     if os.path.isdir(api_private):
         for fn in os.listdir(api_private):
@@ -181,21 +190,24 @@ def copy_api():
     if os.path.isdir(api_public):
         create_zip(api_public, 'att/{}.zip'.format(task_name))
 
+statement_languages = None
 
 def write_yaml():
     data = OrderedDict()
     data['name'] = task_name
     data['title'] = get_config_value('TITLE', default=task_name)
-    data['n_input'] = sum(len(g.cases) for g in tests)
-    data['public_testcases'] = ','.join(str(c.index) for g in tests for c in g.cases if c.public)
+    data['n_input'] = len(cases)
+    data['public_testcases'] = ','.join(str(c.index) for c in cases if c.public)
     data['time_limit'] = get_config_value('TIME_LIMIT')
     data['memory_limit'] = get_config_value('MEMORY_LIMIT')
     data['infile'] = get_config_value('INPUT_FILE', {'taskname': task_name}, default='')
     data['outfile'] = get_config_value('OUTPUT_FILE', {'taskname': task_name}, default='')
-    data['primary_language'] = get_config_value('PRIMARY_STATEMENT', default='lt')
+    if statement_languages is not None:
+        data['statement_languages'] = statement_languages
+    data['primary_language'] = get_config_value('PRIMARY_STATEMENT', default='en')
     for k, v in get_config_value('PROPERTIES', default={}).iteritems():
         data[k] = v
-    with open('{}.yaml'.format(task_name), 'w') as f:
+    with open('task.yaml', 'w') as f:
         #yaml.dump(data, f, default_flow_style=False)
         for k, v in data.iteritems():
             yaml.dump({k: v}, f, default_flow_style=False)
@@ -203,9 +215,10 @@ def write_yaml():
 
 
 def run():
+    global statement_languages
     create_dirs()
     copy_test_files()
-    copy_task()
+    statement_languages = copy_task()
     write_scores()
     copy_api()
     write_yaml()
